4ee1974c7d087bfabdfa6e79a96f41055689c661
krishna-subramanian <Krishna.Subramanian@devolo.de>
2021-12-22 13:55:19 +0100

    Inject repository dependencies in InfoContributor
    TargetManagement, RolloutManagement, and DistributionSetManagement

--- hawkbit-runtime/hawkbit-update-server/src/main/java/org/eclipse/hawkbit/app/CustomInfoContributor.java
+++ hawkbit-runtime/hawkbit-update-server/src/main/java/org/eclipse/hawkbit/app/CustomInfoContributor.java
@@ -1,5 +1,8 @@
 package org.eclipse.hawkbit.app;
 
+import org.eclipse.hawkbit.repository.DistributionSetManagement;
+import org.eclipse.hawkbit.repository.RolloutManagement;
+import org.eclipse.hawkbit.repository.TargetManagement;
 import org.springframework.boot.actuate.info.Info;
 import org.springframework.boot.actuate.info.InfoContributor;
 import org.springframework.stereotype.Component;
@@ -7,6 +10,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class CustomInfoContributor implements InfoContributor {
 
+    private final TargetManagement targetManagement;
+    private final DistributionSetManagement distributionSetManagement;
+    private final RolloutManagement rolloutManagement;
+
+    public CustomInfoContributor(TargetManagement targetManagement, DistributionSetManagement distributionSetManagement, RolloutManagement rolloutManagement) {
+        this.targetManagement = targetManagement;
+        this.distributionSetManagement = distributionSetManagement;
+        this.rolloutManagement = rolloutManagement;
+    }
     @Override
     public void contribute(Info.Builder builder) {
         builder.withDetail("sampleData", "Hello World!");
