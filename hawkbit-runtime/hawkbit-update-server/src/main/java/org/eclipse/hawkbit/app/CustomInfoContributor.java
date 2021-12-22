dd783f6ef7030781d72f16433b6326e521d1c647
krishna-subramanian <Krishna.Subramanian@devolo.de>
2021-12-22 13:57:13 +0100

    Expose targets by status to /info endpoint

--- hawkbit-runtime/hawkbit-update-server/src/main/java/org/eclipse/hawkbit/app/CustomInfoContributor.java
+++ hawkbit-runtime/hawkbit-update-server/src/main/java/org/eclipse/hawkbit/app/CustomInfoContributor.java
@@ -3,9 +3,12 @@ package org.eclipse.hawkbit.app;
 import org.eclipse.hawkbit.repository.DistributionSetManagement;
 import org.eclipse.hawkbit.repository.RolloutManagement;
 import org.eclipse.hawkbit.repository.TargetManagement;
+import org.eclipse.hawkbit.repository.model.TargetUpdateStatus;
 import org.springframework.boot.actuate.info.Info;
 import org.springframework.boot.actuate.info.InfoContributor;
 import org.springframework.stereotype.Component;
+
+import java.util.*;
 
 @Component
 public class CustomInfoContributor implements InfoContributor {
@@ -21,7 +24,21 @@ public class CustomInfoContributor implements InfoContributor {
     }
     @Override
     public void contribute(Info.Builder builder) {
-        builder.withDetail("sampleData", "Hello World!");
+        Map<String, Object> targetsByState = new HashMap<String, Object>();
+
+        final List<TargetUpdateStatus> pendingTargets = Collections.singletonList(TargetUpdateStatus.PENDING);
+        final List<TargetUpdateStatus> unknownTargets = Collections.singletonList(TargetUpdateStatus.UNKNOWN);
+        final List<TargetUpdateStatus> errorTargets = Collections.singletonList(TargetUpdateStatus.ERROR);
+        final List<TargetUpdateStatus> inSyncTargets = Collections.singletonList(TargetUpdateStatus.IN_SYNC);
+        final List<TargetUpdateStatus> registeredTargets = Collections.singletonList(TargetUpdateStatus.REGISTERED);
+
+        targetsByState.put("pending", targetManagement.countByFilters(pendingTargets, null, null, null, Boolean.FALSE));
+        targetsByState.put("unknown", targetManagement.countByFilters(unknownTargets, null, null, null, Boolean.FALSE));
+        targetsByState.put("error", targetManagement.countByFilters(errorTargets, null, null, null, Boolean.FALSE));
+        targetsByState.put("in_sync", targetManagement.countByFilters(inSyncTargets, null, null, null, Boolean.FALSE));
+        targetsByState.put("registered", targetManagement.countByFilters(registeredTargets, null, null, null, Boolean.FALSE));
+
+        builder.withDetail("targets_by_state", targetsByState);
         builder.withDetail("total_targets", targetManagement.count());
         builder.withDetail("offline_targets", targetManagement.countByFilters(null, Boolean.TRUE, null, null, Boolean.FALSE));
     }
