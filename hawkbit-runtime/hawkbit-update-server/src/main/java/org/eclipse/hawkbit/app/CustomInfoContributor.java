7992284d3dfd53582c4a3869b8a724ab63cc06b7
krishna-subramanian <Krishna.Subramanian@devolo.de>
2021-12-22 13:57:44 +0100

    Expose rollouts by status to /info endpoint

--- hawkbit-runtime/hawkbit-update-server/src/main/java/org/eclipse/hawkbit/app/CustomInfoContributor.java
+++ hawkbit-runtime/hawkbit-update-server/src/main/java/org/eclipse/hawkbit/app/CustomInfoContributor.java
@@ -1,11 +1,15 @@
 package org.eclipse.hawkbit.app;
 
 import org.eclipse.hawkbit.repository.DistributionSetManagement;
+import org.eclipse.hawkbit.repository.OffsetBasedPageRequest;
 import org.eclipse.hawkbit.repository.RolloutManagement;
 import org.eclipse.hawkbit.repository.TargetManagement;
+import org.eclipse.hawkbit.repository.model.Rollout;
 import org.eclipse.hawkbit.repository.model.TargetUpdateStatus;
 import org.springframework.boot.actuate.info.Info;
 import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
@@ -22,6 +26,7 @@ public class CustomInfoContributor implements InfoContributor {
         this.distributionSetManagement = distributionSetManagement;
         this.rolloutManagement = rolloutManagement;
     }
+
     @Override
     public void contribute(Info.Builder builder) {
         Map<String, Object> targetsByState = new HashMap<String, Object>();
@@ -39,6 +44,25 @@ public class CustomInfoContributor implements InfoContributor {
         targetsByState.put("registered", targetManagement.countByFilters(registeredTargets, null, null, null, Boolean.FALSE));
 
         builder.withDetail("targets_by_state", targetsByState);
+
+        Map<String, Object> rolloutsByState = new HashMap<String, Object>();
+
+        final Page<Rollout> rolloutPage = rolloutManagement.findAllWithDetailedStatus(new OffsetBasedPageRequest(0, 100, Sort.by(Sort.Direction.ASC, "name")), false);
+        final List<Rollout> rolloutList = rolloutPage.getContent();
+
+        rolloutsByState.put("creating", rolloutList.stream().filter(rollout -> Rollout.RolloutStatus.CREATING.equals(rollout.getStatus())).count());
+        rolloutsByState.put("approval_denied", rolloutList.stream().filter(rollout -> Rollout.RolloutStatus.APPROVAL_DENIED.equals(rollout.getStatus())).count());
+        rolloutsByState.put("deleted", rolloutList.stream().filter(rollout -> Rollout.RolloutStatus.DELETED.equals(rollout.getStatus())).count());
+        rolloutsByState.put("deleting", rolloutList.stream().filter(rollout -> Rollout.RolloutStatus.DELETING.equals(rollout.getStatus())).count());
+        rolloutsByState.put("finished", rolloutList.stream().filter(rollout -> Rollout.RolloutStatus.FINISHED.equals(rollout.getStatus())).count());
+        rolloutsByState.put("paused", rolloutList.stream().filter(rollout -> Rollout.RolloutStatus.PAUSED.equals(rollout.getStatus())).count());
+        rolloutsByState.put("ready", rolloutList.stream().filter(rollout -> Rollout.RolloutStatus.READY.equals(rollout.getStatus())).count());
+        rolloutsByState.put("running", rolloutList.stream().filter(rollout -> Rollout.RolloutStatus.RUNNING.equals(rollout.getStatus())).count());
+        rolloutsByState.put("starting", rolloutList.stream().filter(rollout -> Rollout.RolloutStatus.STARTING.equals(rollout.getStatus())).count());
+        rolloutsByState.put("stopped", rolloutList.stream().filter(rollout -> Rollout.RolloutStatus.STOPPED.equals(rollout.getStatus())).count());
+        rolloutsByState.put("waiting_for_approval", rolloutList.stream().filter(rollout -> Rollout.RolloutStatus.WAITING_FOR_APPROVAL.equals(rollout.getStatus())).count());
+
+        builder.withDetail("rollouts_by_state", rolloutsByState);
         builder.withDetail("total_targets", targetManagement.count());
         builder.withDetail("offline_targets", targetManagement.countByFilters(null, Boolean.TRUE, null, null, Boolean.FALSE));
     }
